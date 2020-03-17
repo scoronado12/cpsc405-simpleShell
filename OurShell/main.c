de9fdd43d6fcd90d0b2ed90f3f01fbab71b67b95
Stefano C. Coronado <stefanocorona12@gmail.com>
2020-03-17 18:02:04 -0400

    tweak

--- OurShell/main.c
+++ OurShell/main.c
@@ -7,12 +7,23 @@ int main(){
 
     while (1){
 
-        printf("%s@nenbarsh$ ", getenv("USER"));
+        printf("[%s@nenbarsh]$ ", getenv("USER"));
         char *cmd = sh_readline();
 
 	    char **cmd_arr = split(cmd, " ");
-	    for (int i = 0; cmd_arr[i] != NULL ; i++)
-		    printf("%s\n", cmd_arr[i]);
+	    for (int i = 0; cmd_arr[i] != NULL ; i++){
+            //printf("%s\n", cmd_arr[i]);
+            if (strcmp(cmd_arr[i],">") == 0){
+                printf("Output redirection\n");
+            }else if (strcmp(cmd_arr[i],"<") == 0){
+                printf("input redirection\n");
+            } else if (strcmp(cmd_arr[i],"|") == 0){
+                printf("pipe!\n");
+            } else if (strcmp(cmd_arr[i], "&")== 0){
+                printf("Background\n");
+            }
+        }
+		    
 
 
 
