863ee2204165d9d9cd7f060ee6c200bcd3b0d928
Stefano C. Coronado <stefanocorona12@gmail.com>
2020-03-17 13:51:40 -0400

    count spaces

--- OurShell/shell.c
+++ OurShell/shell.c
@@ -10,14 +10,14 @@ char *sh_readline(){
 }
 
 char **split(char *str, char *tok){
-	char arr[15];
-	int i = 0;
-	arr[i] = strtok(str, tok);
-	
-	while (arr[i] != NULL){
-		arr[i++] = strtok (NULL, " ");
+	int num_spaces = 0;
+	for (int i = 0 ; strcmp(str[i], '\0'); i++){
+		if (strcmp(str[i], " ")){
+			num_spaces++;
+		}	
 	}
 
-	return arr;	
+	char str_arr[num_spaces + 1];
+	str_arr[num_spaces +1] = NULL;
 
 }
